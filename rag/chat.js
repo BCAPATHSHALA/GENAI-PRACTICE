3ea57a15380582f0138fe7f986f13799cd4d1603
BCAPATHSHALA <MANOJKUMARGFG.1@GMAIL.COM>
2025-08-20 01:16:39 +0530

    chating phase: 8. search relevant vector embedding from vector Database Qdrant DB

--- rag/chat.js
+++ rag/chat.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { OpenAI } from "openai";
 import { OpenAIEmbeddings } from "@langchain/openai";
+import { QdrantVectorStore } from "@langchain/qdrant";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -8,12 +9,9 @@ const openai = new OpenAI({
 
 const main = async () => {
   try {
-    // Step 6: user input query
     const userQuery =
       "please, can you tell me about the hosting concept in node.js";
-    const SYSTEM_PROMPT = `You are an AI assistant that answers questions based on the provided context available to you from a PDF file with the content and page number. Only answer based on the available context from file.
-    
-    Context: ..............`;
+
     const messagesHistory = [
       { role: "system", content: SYSTEM_PROMPT },
       { role: "user", content: userQuery },
@@ -25,7 +23,19 @@ const main = async () => {
       model: "text-embedding-3-large",
     });
 
+    // Step 8: search relevant vector embedding from vector Database Qdrant DB
+    const vectorStore = await QdrantVectorStore.fromExistingCollection(
+      embeddings,
+      {
+        url: "http://localhost:6333",
+        collectionName: "notebookllm",
+      }
+    );
 
+    // Step 6: user input query
+    const SYSTEM_PROMPT = `You are an AI assistant that answers questions based on the provided context available to you from a PDF file with the content and page number. Only answer based on the available context from file.
+    
+    Context: .............`;
     const response = await openai.chat.completions.create({
       model: "gpt-4.1-nano",
       messages: messagesHistory,
