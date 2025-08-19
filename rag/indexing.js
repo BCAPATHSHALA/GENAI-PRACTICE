11741e9648c5a3cfa48235ed64abd1a15f26e0b0
BCAPATHSHALA <MANOJKUMARGFG.1@GMAIL.COM>
2025-08-19 23:56:56 +0530

    indexing phase: 3. split pdf data into chunks

--- rag/indexing.js
+++ rag/indexing.js
@@ -1,4 +1,5 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 const main = async () => {
   try {
@@ -6,8 +7,15 @@ const main = async () => {
     const pdfPath = "./nodejs.pdf";
     const loader = new PDFLoader(pdfPath);
     const docs = await loader.load();
+    console.log("Pages loaded:", docs.length);
 
-    console.log(docs);
+    // Step 3: split pdf data into chunks
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 10000,
+      chunkOverlap: 1000,
+    });
+    const chunks = await splitter.splitDocuments(docs);
+    console.log(`Total chunks: ${chunks.length} | First chunk: ${chunks[0]}`);
   } catch (err) {
     console.log(`Indexing error: ${err}`);
   }
